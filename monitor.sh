b7afc0f098ae7afa74debfcb43795c86cf016a3a
FelipeMaeda <56806716+FelipeMaeda@users.noreply.github.com>
2021-08-25 09:58:22 -0300

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -33,24 +33,24 @@ fi;
 # Verifica se a lista de IPs bloqueados existe
 if [ -e /tmp/ips_atuais.txt ]; then
 	echo "Arquivo existe. Validando se a lista de IPs não está vazia...";
-	count=$(egrep "*" /tmp/ips_atuais.txt | wc -l);
+	count=$(cat /tmp/ips_atuais.txt | egrep "*" | wc -l);
 	if [ $count -eq 0 ]; then
 		echo "Arquivo de IPs está vazio. Inicializando nova contagem de IPs";
-		iptables -S | grep DROP | awk '{ print $4 }' >> /tmp/ips_atuais.txt;
+		iptables -S | egrep "DROP|REJECT" | awk '{ print $4 }' >> /tmp/ips_atuais.txt;
 	fi;
 else
 	echo "Arquivo não existe, reset de IPs efetuados. Inicializando nova contagem de IPs";
-	iptables -S | grep DROP | awk '{ print $4 }' >> /tmp/ips_atuais.txt;
+	iptables -S | egrep "DROP|REJECT" | awk '{ print $4 }' >> /tmp/ips_atuais.txt;
 fi;
 
 # Verifica se houve novos bloqueios
-count_ips=$(iptables -S | grep DROP | awk '{ print $4 }' | egrep -v "$(cat /tmp/ips_atuais.txt | paste -sd'|')" | wc -l)
+count_ips=$(iptables -S | egrep "DROP|REJECT" | awk '{ print $4 }' | egrep -v "$(cat /tmp/ips_atuais.txt | paste -sd'|')" | wc -l)
 
 if [ $count_ips -eq 0 ]; then
 	echo -e "Não houve novos bloqueios de IP em sua estrutura.\n\nAtenciosamente,\n$SIGNATURE" >> /tmp/sendmail.txt;
 else
 	echo -e "Segue IPs bloqueados em sua estrutura:\n" >> /tmp/sendmail.txt;
-	iptables -S | grep DROP | awk '{ print $4 }' | egrep -v "$(cat /tmp/ips_atuais.txt | paste -sd'|')" | tee -a /tmp/ips_atuais.txt >> /tmp/sendmail.txt;
+	iptables -S | egrep "DROP|REJECT" | awk '{ print $4 }' | egrep -v "$(cat /tmp/ips_atuais.txt | paste -sd'|')" | tee -a /tmp/ips_atuais.txt >> /tmp/sendmail.txt;
 	echo -e "\nAtenciosamente,\n$SIGNATURE" >> /tmp/sendmail.txt;
 fi;
 
